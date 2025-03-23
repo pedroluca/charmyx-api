bf27db5043b74f1f963ee2a4e6a8632aa728ad48
Carlos Manoel <carlosmanoel1352002@gmail.com>
2025-03-23 17:34:42 -0300

    feat: Adicionando PUT e DELETE de agendamento

--- charmyxapi/agendamento/views.py
+++ charmyxapi/agendamento/views.py
@@ -7,7 +7,7 @@ from .models import Agendamento
 from .serializers import AgendamentoSerializer
 
 class AgendamentoAPI(APIView):
-    #permission_classes = [IsAuthenticated]  # Apenas usuários autenticados podem acessar
+    permission_classes = [IsAuthenticated]  # Apenas usuários autenticados podem acessar
 
     def get(self, request, agendamento_id=None):
         """Lista todos os agendamentos ou retorna um único por ID"""
@@ -30,3 +30,19 @@ class AgendamentoAPI(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, agendamento_id):
+        """Atualiza um agendamento existente"""
+        agendamento = get_object_or_404(Agendamento, id=agendamento_id)
+        serializer = AgendamentoSerializer(agendamento, data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, agendamento_id):
+        """Exclui um agendamento específico"""
+        agendamento = get_object_or_404(Agendamento, id=agendamento_id)
+        agendamento.delete()
+        return Response({"message": "Agendamento deletado com sucesso!"}, status=status.HTTP_204_NO_CONTENT)
