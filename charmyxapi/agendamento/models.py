d3954cc895656ae81f2927ddabddd04774f1137f
Carlos Manoel <carlosmanoel1352002@gmail.com>
2025-03-22 23:39:59 -0300

    feat: adicionar agendamento e charmyxapi/settings

---
+++ charmyxapi/agendamento/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Agendamento(models.Model):
+    horario = models.TimeField()
+    data = models.DateField()
+    observacao = models.TextField()
+    STATUS_CHOICES = [
+        ('PEN', 'Pendente'),
+        ('CON', 'Confirmado'),
+        ('FIN', 'Concluído'),
+    ]
+    status = models.CharField(
+        max_length=3,
+        choices=STATUS_CHOICES,
+        default='PEN',
+        verbose_name="Status do Serviço",
+        help_text="Estado atual do serviço"
+    )
+    # servico_id = models.ForeignKey(Servico, on_delete=models.CASCADE)
+    # cliente_id = models.ForeignKey(Cliente, on_delete=models.CASCADE)
--- charmyxapi/charmyxapi/settings.py
+++ charmyxapi/charmyxapi/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     # 'cliente',
     'produto',
     # 'agendamento',
+    'agendamento',
     # 'servico',
+    
 ]
 
 MIDDLEWARE = [
