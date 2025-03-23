fa38266e850b37e19f8db888bf696afd0104076a
Pietra Geovanna M. Ferreira <pietrag89@gmail.com>
2025-03-23 17:59:57 -0300

    chore: remove suporte à autenticação via Basic Auth

--- charmyxapi/charmyxapi/settings.py
+++ charmyxapi/charmyxapi/settings.py
@@ -141,7 +141,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
